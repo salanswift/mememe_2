44f1c65378d1d582524b664c8512e247ffeb09ca
salanswift <arakhtar@icloud.com>
2015-05-06 01:33:42 +0500

    Committed by Udacity

---
+++ MEME me/MEME me/MEMECollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  VillainCollectionViewCell.swift
+//  BondVillains
+//
+//  Created by Gabrielle Miller-Messner on 2/3/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class MEMECollectionViewCell: UICollectionViewCell {
+ 
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+}
